061eb25d53660da2e4d981f2497357e1c680a7b3
djdagorne <dj.dagorne@gmail.com>
2019-06-10 19:33:21 -0400

    adding code from laptop

---
+++ index.js
@@ -0,0 +1,39 @@
+/* //VARIABLES
+var qNum = 0; //keeps track of which question/answer object to render
+
+var ansKey = []; //an array of which multiple choice answer was correct.
+
+var userAns = ''; //keeps track of which answer was inputted last
+
+var userScore = ''; //keeps track of how many questions were correct overall
+
+const qList = [{},{},{}]; //an array of objects with questions and potential answers which will be wrapped within <li> elements general HTML/CSS
+
+
+function startQuiz(){
+  //loads a display screen with logo, information on what the quiz will be about
+}
+
+function displayQuestion(){
+  //renders a question page based on the users progress 
+  //questions+answers are objects kept in an array, <li> elements and radio form will be generated based on progression through the index of the array.
+
+  //if user is on question 1, use index 0 of the array etc.
+
+  //qNum++
+  //if correct answer >> userScore++
+}
+
+function submitAnswer(){
+  //will store the selected answer into userAns
+}
+
+function userScore(qNum, ansKey, userAns){
+  //will compare the userAns to the ansKey, and display how many answers were correct.
+
+  //answerList will be an array of answer values. the index of said array will be the same value as qNum, as to keep track of which answers are correct.
+}
+
+function finalScreen(){
+  //will display final user score, as well as a button/way to restart the quiz from the beginning. 
+} */
