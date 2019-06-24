e04ff57c52831dc98a6258b3c8b167859670d975
Dexter Dagorne <dj.dagorne@gmail.com>
2019-06-24 11:47:41 -0400

    functionality looking good

---
+++ data.js
@@ -0,0 +1,132 @@
+const STORE = [
+    {
+      question: 'Which year did Mario first appear in a video game?',
+      answers: [
+        '1981',
+        '1982',
+        '1985',
+        '1992'
+        ],
+      correctAnswer: '1981',
+      blurb: 'Mario\'s first appearance in a Nintendo game was arcade classic Donkey Kong',
+      pic: 'imgs/question1.jpg',
+      alt: 'screenshot of Donkey Kong'
+    },
+    {
+      question: 'What job has Mario not had?',
+      answers: [
+        'Carpenter',
+        'Race Car Driver',
+        'Doctor',
+        'Tattoo Artist'
+        ],
+      correctAnswer: 'Tattoo Artist',
+      blurb: 'According to Marios creator, he was supposed to be a carpenter in Donkey Kong, and only later became a plumber when pipes were added to the games as a means of changing levels.',
+      pic: 'imgs/question2.png',
+      alt: 'Mario in Mario Kart 8'
+    },
+    {
+      question: 'When did Bowser first appear?',
+      answers: [
+        '1981',
+        '1983',
+        '1985',
+        '1992'
+        ],
+      correctAnswer: '1985',
+      blurb: 'Bowser first appeared as King Koopa in 1985\'s Super Mario Bros.',
+      pic: 'imgs/question3.jpg',
+      alt: 'Final boss of Super Mario Bros.'
+    },
+    {
+      question: 'When was Nintendo founded?',
+      answers: [
+        '1974',
+        '1978',
+        '1620',
+        '1889'
+        ],
+      correctAnswer: '1889',
+      blurb: 'Nintendo was originally started as a playing card company all the way back in 1889.',
+      pic: 'imgs/question4.jpg',
+      alt: 'Nintendo playing cards'
+    },
+    {
+      question: 'What is Nintendo\'s best selling console?',
+      answers: [
+        'Wii',
+        'Gameboy',
+        'Nintendo DS',
+        'Nintendo Switch'
+        ],
+      correctAnswer: 'Nintendo DS',
+      blurb: 'Nintendo DS has sold over 154 Million units since 2004.',
+      pic: 'imgs/question5.jpg',
+      alt: 'Variation of the Nintendo DS'
+    },
+    {
+      question: 'What is Nintendo\'s least popular console?',
+      answers: [
+        'Gameboy Advance',
+        'Switch',
+        'Nintendo 64',
+        'Gamecube'
+        ],
+      correctAnswer: 'Gamecube',
+      blurb: 'The Gamecube only sold 21 million units.',
+      pic: 'imgs/question6.jpg',
+      alt: 'Nintendo Gamecube'
+    },
+    {
+      question: 'What year did Legend of Zelda first release?',
+      answers: [
+        '1991',
+        '1983',
+        '1986',
+        '1992'
+        ],
+      correctAnswer: '1986',
+      blurb: 'The Legend of Zelda first came out in Febraury 21st 1986.',
+      pic: 'imgs/question7.jpg',
+      alt: 'Starting Screen of Legend of Zelda'
+    },
+    {
+      question: 'Who is the current president of Nintendo of America?',
+      answers: [
+        'Reggie Fils-Aimé',
+        'Shigeru Miyamoto',
+        'Satoru Iwata',
+        'Doug Bowser'
+        ],
+      correctAnswer: 'Doug Bowser',
+      blurb: 'Through some weird conincidence, the president of Nintendos American HQ is actually named Doug Bowser. What are the odds?',
+      pic: 'imgs/question8.jpg',
+      alt: 'Mario and the other Bowser.'
+    },
+    {
+      question: 'What was Nintendo\'s first 3D game?',
+      answers: [
+        'X',
+        'Super Mario 64',
+        'Star Fox',
+        'Legend of Zelda Ocarina of Time'
+        ],
+      correctAnswer: 'X',
+      blurb: 'The answer is a Japan only game called X in 1992, and it was a first person shooter built on a wireframe engine.',
+      pic: 'imgs/question9.jpg',
+      alt: 'Screencapture of X for the Gameboy.'
+    },
+    {
+      question: 'After Mario, what is Nintendo\'s most successful property?',
+      answers: [
+        'Wii Sports',
+        'Legend of Zelda',
+        'Super Smash Brothers',
+        'Pokemon'
+        ],
+      correctAnswer: 'Pokemon',
+      blurb: 'Pokemon has an estimated 309 million titles sold across all Nintendo platforms... and thats not counting Pokemon Go\'s 1 billion downloads!',
+      pic: 'imgs/question10.png',
+      alt: 'Pokemon Logo'
+    },
+]
--- index.js
+++ index.js
@@ -1,39 +1,142 @@
-/* //VARIABLES
-var qNum = 0; //keeps track of which question/answer object to render
+//VARIABLES
+var qNum = 9; //keeps track of which question/answer object to render
+var userScore = 0; //keeps track of how many questions were correct overall
 
-var ansKey = []; //an array of which multiple choice answer was correct.
-
-var userAns = ''; //keeps track of which answer was inputted last
-
-var userScore = ''; //keeps track of how many questions were correct overall
-
-const qList = [{},{},{}]; //an array of objects with questions and potential answers which will be wrapped within <li> elements general HTML/CSS
-
-
-function startQuiz(){
-  //loads a display screen with logo, information on what the quiz will be about
+function startQuiz(event){
+  $('main').html(`<p class="start-quiz-blerb">Test your Nintendo knowledge with this 10 question Quiz! Hit the button below to start!</p>
+    <button class="start-quiz-button">Start Quiz</button>
+    <div class='question-area'></div>`);
+    
+  $('.startArea').on('click', '.start-quiz-button', function () {    
+    $('.start-quiz-blerb').remove();
+    $('.start-quiz-button').remove();
+    renderQuestion();
+  });
 }
 
-function displayQuestion(){
-  //renders a question page based on the users progress 
-  //questions+answers are objects kept in an array, <li> elements and radio form will be generated based on progression through the index of the array.
-
-  //if user is on question 1, use index 0 of the array etc.
-
-  //qNum++
-  //if correct answer >> userScore++
+function renderQuestion () {
+  $('.post-question-box').remove();
+  $('.question-area').html(generateQuestion(STORE));
+  submitAnswer();
 }
 
 function submitAnswer(){
-  //will store the selected answer into userAns
+  $('form').on('submit', function () {
+    event.preventDefault();
+    userScoreTracker();
+  });
 }
 
-function userScore(qNum, ansKey, userAns){
-  //will compare the userAns to the ansKey, and display how many answers were correct.
+function userScoreTracker(){
+  var userAnswer = 'input[name=user-choice]:checked';
+  if($(userAnswer).val() === STORE[qNum].correctAnswer){
+    correctAnswerScreen();
+  }else{
+    wrongAnswerScreen();
+  }
+}
 
-  //answerList will be an array of answer values. the index of said array will be the same value as qNum, as to keep track of which answers are correct.
+function correctAnswerScreen(){
+  ++userScore;
+  $('main').html(`
+  <div class="post-question-box">
+    <div class="stats">
+      <span class="question-${qNum+1}">Question: ${qNum+1}/10</span>
+      <span class="userScore">Score: ${userScore}/10</span>
+    </div>
+    <h2>Correct!</h2>
+    <img alt=${STORE[qNum].alt} class="answer-img" src="${STORE[qNum].pic}"></img>
+    <p class="display-answer">${STORE[qNum].blurb}</p>
+    <button class="next-question-button">${qNum+1 == STORE.length ? 'Results' : 'Next Question' }</button>
+  </div>
+  <div class='question-area'></div>`);
+  $('.post-question-box').on('click', '.next-question-button', function(){      
+    if(qNum+1 < STORE.length){
+      qNum++;
+      renderQuestion();
+    }else{
+      finalScreen();
+    }
+  });
+}
+
+function wrongAnswerScreen(){
+  $('main').html(`
+    <div class="post-question-box">
+    <div class="stats">
+      <span class="question-${qNum+1}">Question: ${qNum+1}/10</span>
+      <span class="userScore">Score: ${userScore}/10</span>
+    </div>
+    <h2>Incorrect!</h2>
+    <img alt=${STORE[qNum].alt} class="answer-img" src="${STORE[qNum].pic}"></img>
+    <p class="display-answer">The correct answer was: ${STORE[qNum].correctAnswer}... ${STORE[qNum].blurb}</p>
+    <button class="next-question-button">Next Question</button>
+    </div>
+    <div class='question-area'></div>`);  
+  $('.post-question-box').on('click', '.next-question-button', function(){
+    qNum++; 
+    renderQuestion();
+  });
+}
+
+function generateQuestion(qList){
+  return `
+  <div class="stats">
+    <span class="question-${qNum+1}">Question: ${qNum+1}/10</span>
+    <span class="userScore">Score: ${userScore}/10</span>
+  </div>
+  <h2 class="question-area">${qList[qNum].question}</h2>
+  <div class="question-answer-area">
+    <form class="answer-area">
+    <fieldset class="fieldset-answers">
+      <label><input type="radio" name="user-choice" value="${qList[qNum].answers[0]}" required>${qList[qNum].answers[0]}</label><br>
+      <label><input type="radio" name="user-choice" value="${qList[qNum].answers[1]}" required>${qList[qNum].answers[1]}</label><br>
+      <label><input type="radio" name="user-choice" value="${qList[qNum].answers[2]}" required>${qList[qNum].answers[2]}</label><br>
+      <label><input type="radio" name="user-choice" value="${qList[qNum].answers[3]}" required>${qList[qNum].answers[3]}</label><br>   
+      <button type='submit' class="submit-answer-button">Submit Answer</button>
+    </fieldset>
+    </form>
+  </div>`;
 }
 
 function finalScreen(){
-  //will display final user score, as well as a button/way to restart the quiz from the beginning. 
-} */
+  let message;  
+  if (userScore>8){
+    message = `Congratulations You got ${userScore} points! You clearly know your Nintendo!`;
+  }
+  else if(userScore<=8 && userScore>=5){
+    message = `Congratulations You got ${userScore} points! But you can do better next time!`;
+  }
+  else{
+    message = `Tough break! You got ${userScore}/10, you can do better next time!`;
+  }
+
+  renderFeedback(message);
+  handleClickRestart();
+}
+
+function handleClickRestart() {
+  $('.quiz-end-area').on('click', '.restart-quiz-button', function () {
+    restartQuiz(); 
+  });
+}
+
+function renderFeedback(message) {
+    $('main').html(generateHTMLFeedback(message));
+}
+
+function generateHTMLFeedback(message) {
+  return `<H2 class="quiz-end-area">
+    <p>${message}</p>
+    <button class='restart-quiz-button'>Restart Quiz</button> 
+    </H2>`;
+}
+
+
+function restartQuiz(){
+  qNum = 0;
+  userScore = 0;
+  startQuiz();
+};
+
+$(startQuiz);
